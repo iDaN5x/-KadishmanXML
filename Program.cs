2f5237779ce6c073716a948e823db4896ee45623
Idan Asraf <idan.asraf@outlook.com>
2021-04-30 13:58:17 +0300

    banana janana

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace KadishmanXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get sitemap file path from arguments.
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine(
+                    "path to sitemap.xml file must be provided as argument.");
+
+                Environment.ExitCode = -1;
+                return;
+            }
+
+            var siteMapFilePath = args[0];
+
+            // Get path of the output file.
+            String outputFilePath;
+
+            if (args.Length == 2)
+            {
+                outputFilePath = args[1];
+            }
+            else
+            {
+                var sourceDir = Path.GetDirectoryName(siteMapFilePath);
+                outputFilePath = Path.Join(sourceDir, "urls.csv");
+            } 
+                    
+            // Extract the URLs from the sitemap into the output file.
+            var extractor = new SiteMapUrlExtractor();
+
+            try
+            {
+                extractor.Extract(siteMapFilePath, outputFilePath);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                Environment.ExitCode = -1;
+                return;
+            }
+
+            // Announce successful exection.
+            Console.WriteLine($"Succesfuly extracted urls into file '{outputFilePath}'");
+        }
+    }
+}
---
+++ SiteMapUrlExtractor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace KadishmanXML
+{
+    class SiteMapUrlExtractor
+    {
+        public void Extract(string siteMapFilePath, string outputFilePath)
+        {
+            // Check sitemap file exists.
+            bool fileExists = !string.IsNullOrWhiteSpace(siteMapFilePath)
+                && File.Exists(siteMapFilePath);
+
+            if (!fileExists)
+            {
+                throw new ArgumentException(
+                    nameof(siteMapFilePath),
+                    "Please provide a valid path to a SiteMap XML file.");
+            }
+
+            // Load site-map xml file.
+            XmlDocument xmlDocument = new XmlDocument();
+            
+
+            try
+            {
+                xmlDocument.Load(siteMapFilePath);
+            }
+            catch (XmlException e)
+            {
+                throw new Exception("File provided is not a valid XML.");
+            }
+
+            // Get all urls from sitemap (lazy).
+            var urls = xmlDocument
+                .SelectNodes("//*[local-name()='urlset']/*[local-name()='url']/*[local-name()='loc']")
+                .Cast<XmlNode>()
+                .Select(node => node.InnerText)
+                .Select(HttpUtility.UrlDecode);
+
+            // Write all urls to the file.
+            File.WriteAllLines(outputFilePath, urls, Encoding.Unicode);
+
+        }
+    }
+}
